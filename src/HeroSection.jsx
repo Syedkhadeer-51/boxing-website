979e6c1095fddbfcd9925f7fab655acee10cb3cb
Syedkhadeer-51 <syedkhadeer4623@gmail.com>
2023-05-17 17:38:52 +0530

    add: contact and hero section

---
+++ src/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='main-container hero-section p-0 m-0 d-flex align-items-center' id='hero-section'>
+        <div className="row p-3 my-2 d-flex justify-content-lg-start justify-content-md-center justify-content-sm-center align-items-center">
+            <div className="col-lg-6 col-md-6 col-12">
+
+        <div className="text-center hero-heading">LEARN BOXING FIGHT LIKE CHAMPIONS</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
