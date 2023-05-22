e51bd187d57acaf5324bfbacc28e6ac1b3d87166
Syedkhadeer-51 <syedkhadeer4623@gmail.com>
2023-05-22 20:19:53 +0530

    adds contact seciton

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import Review from './Review'
 import Insta from './Insta'
 import Contact from './Contact'
 import HeroSection from './HeroSection'
+import Actions from './Actions'
+import Navbar from './Navbar'
 
 function App() {
 
 
   return (
     <>
+    <Navbar/>
     <HeroSection/>
     <About/>
     <Packages/>
     <Review/>
+    <Actions/>
     <Insta/>
     <Contact/>
     </>
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
         <div className="main-container p-0 m-0" id='contact'>
             <div className="row p-3 my-2 d-flex justify-content-center align-items-center">
-                <div className='col-md-6 col-12 image-section animate__animated animate__fadeInLeft d-flex align-items-center justify-content-center'>
+                <div className='col-md-6 col-12 contact-img-column image-section animate__animated animate__fadeInLeft d-flex align-items-center justify-content-center'>
                     <div className='text-center p-3 m-2'>
                         <img className='img-fluid' src={contactImg} alt="about-img" />
                     </div>
@@ -17,31 +17,60 @@ const Contact = () => {
                         a supportive community of other members
                     </div>
                     <div className='text-center my-3 w-50'>
-                        <button type="button" className="btn prinary-btn w-100" data-bs-toggle="modal" data-bs-target="#contactModal">CONTACT</button>
+                        <button type="button" className="btn primary-btn w-100" data-bs-toggle="modal" data-bs-target="#contactModal">CONTACT</button>
                     </div>
                 </div>
-                <div className="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+                <div className="modal fade" id="contactModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel"
                     aria-hidden="true">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content py-2">
+                            <h4 className="font-weight-bold form-main-txt text-center">Get in touch with us</h4>
                             <div className="modal-header text-center">
-                                <h4 className="modal-title w-100 font-weight-bold">Get in touch with us</h4>
+                                <div className="row d-flex justify-content-md-between justify-content-center">
+                                    <div className="col-md-3 col-6 p-1 m-1 text-center">
+                                        <span class="material-symbols-outlined p-2 rounded-pill form-icons text-white mx-1 my-3">
+                                            home
+                                        </span>
+                                        <p className='p-0 m-0 form-header-text'>
+                                            HSR layout 27 th main road, Bangalore
+                                        </p>
+                                    </div>
+                                    <div className="col-md-3 col-6 p-1 m-1 text-center">
+                                    <span class="material-symbols-outlined p-2 rounded-pill form-icons text-white mx-1 my-3">
+                                            call
+                                        </span>
+                                        <p className='p-0 m-0 form-header-text'>
+                                            7598318996
+                                        </p>
+                                    </div>
+                                    <div className="col-md-3 col-6 p-1 m-1 text-center">
+                                    <span class="material-symbols-outlined p-2 rounded-pill form-icons text-white mx-1 my-3">
+                                            mail
+                                        </span>
+                                        <p className='p-0 m-0 form-header-text'>
+                                            kartickchamp 46@ gmail.com
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                             <div className="modal-body mx-3">
                                 <div className="md-form mb-5">
                                     <i className="fas fa-user prefix grey-text"></i>
-                                    <input type="text" id="form34" className="form-control validate" placeholder='Name'/>
+                                    <input type="text" id="form34" className="form-control validate" placeholder='Name' />
                                 </div>
 
                                 <div className="md-form mb-5">
                                     <i className="fas fa-envelope prefix grey-text"></i>
-                                    <input type="email" id="form29" className="form-control validate" placeholder='Email'/>
+                                    <input type="email" id="form29" className="form-control validate" placeholder='Email' />
                                 </div>
                                 <div className="md-form">
                                     <i className="fas fa-pencil prefix grey-text"></i>
                                     <textarea type="text" id="form8" className="md-textarea form-control" rows="4" placeholder='Message'></textarea>
                                 </div>
                             </div>
+                            <div className='text-center m-3 w-70 '>
+                        <button type="button" className="btn primary-btn w-100">SEND</button>
+                    </div>
                         </div>
                     </div>
                 </div>
